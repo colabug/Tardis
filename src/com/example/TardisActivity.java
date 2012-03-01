2fc8dc4f7ee9a057ffe5cfe2f7a20e51e0e74f18
Corey Leigh Latislaw <colabug@gmail.com>
2012-03-01 14:56:00 -0500

    Moved clicky code to fragment
    * Moved code to fragment with these updates:
        - Used getActivity() instead of a reference to TardisActivity.this
        - Used layout.findViewById() instead normal findViewById()

--- src/com/example/TardisActivity.java
+++ src/com/example/TardisActivity.java
@@ -1,10 +1,7 @@
 package com.example;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
 
 public class TardisActivity extends Activity
 {
@@ -16,17 +13,5 @@ public class TardisActivity extends Activity
 
         // Associate a view with this screen
         setContentView(R.layout.main);
-
-        // Get handle on the image view for click events
-        ImageView tardisButton = (ImageView)findViewById(R.id.tardis);
-        tardisButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick (View view)
-            {
-                // Show the inside of the Tardis
-                startActivity(new Intent(TardisActivity.this, InsideTardisActivity.class));
-            }
-        });
     }
 }
--- src/com/example/TardisFragment.java
+++ src/com/example/TardisFragment.java
@@ -1,10 +1,12 @@
 package com.example;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class TardisFragment extends Fragment
@@ -13,6 +15,19 @@ public class TardisFragment extends Fragment
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
         LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.tardis, container, false);
+
+        // Get handle on the image view for click events
+        ImageView tardisButton = (ImageView)layout.findViewById(R.id.tardis);
+        tardisButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick (View view)
+            {
+                // Show the inside of the Tardis
+                startActivity(new Intent(getActivity(), InsideTardisActivity.class));
+            }
+        });
+
         return layout;
     }
 }
