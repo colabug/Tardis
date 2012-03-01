9905ec42004463479bd66fd1773d7386b11625d3
Corey Leigh Latislaw <colabug@gmail.com>
2012-03-01 14:48:27 -0500

    Reused main xml for the view of this fragment

--- src/com/example/TardisFragment.java
+++ src/com/example/TardisFragment.java
@@ -5,12 +5,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class TardisFragment extends Fragment
 {
     @Override
     public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
     {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        LinearLayout layout = (LinearLayout) inflater.inflate(R.layout.tardis, container, false);
+        return layout;
     }
 }
