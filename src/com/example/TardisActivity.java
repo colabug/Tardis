974aa9435788dd4f2ea95e18f3a7d5270c6ecd57
Corey Leigh Latislaw <colabug@gmail.com>
2012-03-01 15:01:25 -0500

    Working Fragment!
    Changed Activity -> Fragment Activity to fix crash

--- src/com/example/TardisActivity.java
+++ src/com/example/TardisActivity.java
@@ -1,9 +1,9 @@
 package com.example;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class TardisActivity extends Activity
+public class TardisActivity extends FragmentActivity
 {
     /** Called when the activity is first created. */
     @Override
